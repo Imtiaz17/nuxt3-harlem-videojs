14bdd1eb5a5e259a6488f859f21dc40bd3929a86
imtiaz17 <imtcse@gmail.com>
2022-11-09 17:19:15 +0600

    intigrated videojs in a video

--- app.vue
+++ app.vue
@@ -1,27 +1,34 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <video-player :options="videoOptions" />
   </div>
 </template>
 <script setup>
-import {
-    computed
-} from 'vue';
+import { computed } from "vue";
 
-import {
-    state,
-    fullName,
-    setFirstName,
-    setLastName
-} from './stores/index';
+import { state, fullName, setFirstName, setLastName } from "./stores/index";
 
 const firstName = computed({
-    get: () => state.firstName,
-    set: value => setFirstName(value)
+  get: () => state.firstName,
+  set: (value) => setFirstName(value),
 });
 
 const lastName = computed({
-    get: () => state.lastName,
-    set: value => setLastName(value)
+  get: () => state.lastName,
+  set: (value) => setLastName(value),
 });
+const videoOptions = 
+  {
+    autoplay: true,
+    controls: true,
+    loadingSpinner: false,
+    errorDisplay: false,  
+    sources: [
+      {
+        src: "http://techslides.com/demos/sample-videos/small.mp4",
+        type: "video/mp4",
+      },
+    ],
+  }
+;
 </script>
---
+++ components/VideoPlayer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <video ref="videoPlayer" class="video-js"></video>
+  </div>
+</template>
+<script setup>
+import videojs from "video.js";
+import 'video.js/dist/video-js.css';
+import { ref, onMounted, onBeforeMount } from "vue";
+let videoPlayer = ref(null);
+const props = defineProps(["options"]);
+let player = null;
+onMounted(() => {
+  let all = videoPlayer.value;
+  console.log(all)
+  player = videojs(all, props.options, () => {
+    player.log("onPlayerReady", player);
+  });
+});
+onBeforeMount(() => {
+  if (player) {
+    player.dispose();
+  }
+});
+</script>
